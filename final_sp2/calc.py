a7eacf1cd074c3a2b21329c2faf41b7486774946
maybeYOLO <maybeyolo1@mail.ru>
2023-05-30 06:38:04 +0500

    fin calc

--- final_sp2/calc.py
+++ final_sp2/calc.py
@@ -1,6 +1,5 @@
-# ID=87769572
+# ID=87807618
 
-from typing import Union
 from operator import add, sub, mul, floordiv
 
 OPERATORS = {
@@ -9,7 +8,6 @@ OPERATORS = {
     '*': mul,
     '/': floordiv
 }
-ORD_TO_INT = ord('0')
 
 
 class Stack():
@@ -31,39 +29,16 @@ class StackUnderflowException(Exception):
 
 def calculate(expr: str) -> int:
     result = 0
-    value = 0
-    value_met = False
-    minus_met = False
-    operator_met = False
-    symbol_operator = ''
     stack = Stack()
-    """
-    Действие выполняется когда после числа или оператора встречается пробел
-    Для выполнения действия над последним числом или оператором,
-    к выражению добавляется оконечный пробел
-    """
-    for symbol in expr + ' ':
-        if symbol in OPERATORS:
-            operator_met = True
-            symbol_operator = symbol
-            if symbol == '-':
-                minus_met = True
-        elif '0' <= symbol <= '9':
-            value = value * 10 + ord(symbol) - ORD_TO_INT
-            value_met = True
-        elif symbol == ' ':
-            if value_met:
-                result = -value if minus_met else value
-                stack.push(result)
-                value = 0
-                value_met = False
-            elif operator_met:
-                operand2nd = stack.pop()
-                operand1st = stack.pop()
-                result = OPERATORS[symbol_operator](operand1st, operand2nd)
-                stack.push(result)
-            operator_met = False
-            minus_met = False
+    for token in expr.split():
+        if token in OPERATORS:
+            operand2nd = stack.pop()
+            operand1st = stack.pop()
+            result = OPERATORS[token](operand1st, operand2nd)
+            stack.push(result)
+        else:
+            result = int(token)
+            stack.push(result)
     return result
 
 
