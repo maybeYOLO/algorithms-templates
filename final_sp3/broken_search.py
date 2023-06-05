8aacd16fd4c017f21bdf1321d50fe817035cf93f
vlad-bravo <17.bravo@gmail.com>
2023-06-05 17:54:17 +0500

    simple

--- final_sp3/broken_search.py
+++ final_sp3/broken_search.py
@@ -3,7 +3,7 @@ from typing import List
 
 def partial_search(nums: List[int], target: int, index: int, half_length: int, full_length: int) -> int:
     if nums[index] == target:
-        return index 
+        return index
     if half_length == 1:
         return -1
     # q
@@ -13,10 +13,6 @@ def broken_search(nums: List[int], target: int) -> int:
     return partial_search(nums, target, 0, len(nums) // 2, len(nums))
 
 
-if __name__ == '__main__':
-    def test() -> None:
-        arr = [19, 21, 100, 101, 1, 4, 5, 7, 12]
-        assert broken_search(arr, 5) == 6
-
-
-    test()
+def test() -> None:
+    arr = [19, 21, 100, 101, 1, 4, 5, 7, 12]
+    assert broken_search(arr, 5) == 6
---
+++ final_sp3/simple_in_place_quick_sort.py
@@ -0,0 +1,6 @@
+def q_sort(nums, left, right) -> None:
+    pivot = (left + right) // 2
+    
+
+nums = [4, 8, 9, 20, 1, 5, 3, 10]
+q_sort(nums, 0, len(nums) - 1)
