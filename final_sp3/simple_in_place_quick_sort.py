c46f0fb7c45d8d347126f4f7b5329c45e326baec
vlad-bravo <17.bravo@gmail.com>
2023-06-06 15:13:38 +0500

    simple+logging

--- final_sp3/simple_in_place_quick_sort.py
+++ final_sp3/simple_in_place_quick_sort.py
@@ -1,30 +1,50 @@
+import logging
+
 def q_sort(nums, left, right, count) -> None:
+    logging.info(f'nums={nums} left={left} right={right} count={count}')
     if left == right:
+        logging.info(f'left={left} == right={right}')
         return
     if right - left == 1:
+        logging.info(f'right={right} - left={left} == 1')
         if nums[left] > nums[right]:
+            logging.info(f'nums[left]={nums[left]} > nums[right]={nums[right]}')
             nums[left], nums[right] = nums[right], nums[left]
         return
     if count > 10:
-        print('count limit')
+        logging.critical('count limit')
         return
     count += 1
     start = left
     end = right
     pivot = nums[left]
-    print(pivot, start, left, right, end, nums)
+    logging.info(f'"{pivot}" {start} {left} {right} {end} {nums}')
     while left < right:
         while nums[left] < pivot and left < right:
             left += 1
-        while nums[right] >= pivot and left < right:
+            logging.info(f'left={left} nums[{left}]={nums[left]}')
+        while nums[right] >= pivot and right > left:
             right -= 1
-        nums[left], nums[right] = nums[right], nums[left]
-        print(pivot, start, left, right, end, nums, 's')
-    print(pivot, start, left, right, end, nums, count)
+            logging.info(f'right={right} nums[{right}]={nums[right]}')
+        if left < right:
+            if nums[left] > nums[right]:
+                nums[left], nums[right] = nums[right], nums[left]
+                logging.info(f'\'{pivot}\' {start} {left} {right} {end} {nums} s')
+            if left < right - 1:
+                left += 1
+            if left < right - 1:
+                right -= 1
+        logging.info(f'`{pivot}` {start} {left} {right} {end} {nums} {count}')
     q_sort(nums, start, left, count)
     q_sort(nums, right, end, count)
     
 
 nums = [4, 8, 9, 20, 1, 5, 3, 10]
+logging.basicConfig(
+    level=logging.INFO,
+    filename="q_sort.log",
+    filemode="w",
+    format="%(message)s"
+)
 q_sort(nums, 0, len(nums) - 1, 0)
 print(nums)
