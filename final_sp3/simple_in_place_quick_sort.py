75093ceeff5d93e4aef0531b76a72068a54571ab
vlad-bravo <17.bravo@gmail.com>
2023-06-06 17:19:11 +0500

    minor changes

--- final_sp3/simple_in_place_quick_sort.py
+++ final_sp3/simple_in_place_quick_sort.py
@@ -1,6 +1,7 @@
 import logging
 
 def q_sort(nums, left, right, count) -> None:
+    logging.info('.' * 60)
     logging.info(f'nums={nums} left={left} right={right} count={count}')
     if left == right:
         logging.info(f'left={left} == right={right}')
@@ -12,18 +13,18 @@ def q_sort(nums, left, right, count) -> None:
             nums[left], nums[right] = nums[right], nums[left]
         return
     if count > 10:
-        logging.critical('count limit')
+        logging.critical('\'count limit\'')
         return
     count += 1
     start = left
     end = right
     pivot = nums[left]
     logging.info(f'"{pivot}" {start} {left} {right} {end} {nums}')
-    while left < right:
-        while nums[left] < pivot and left < right:
+    while left < right - 1:
+        while nums[left] < pivot and left < right - 1:
             left += 1
             logging.info(f'left={left} nums[{left}]={nums[left]}')
-        while nums[right] >= pivot and right > left:
+        while nums[right] >= pivot and right > left+ 1:
             right -= 1
             logging.info(f'right={right} nums[{right}]={nums[right]}')
         if left < right:
